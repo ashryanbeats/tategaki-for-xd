c17d666e1bc30380c574dafcb1ffbface511a97f
Ash <ash@ashryanbeats.com>
2018-10-18 17:08:33 -0700

    Detects if text is vertical.

--- main.js
+++ main.js
@@ -13,32 +13,56 @@ const application = require("application");
 // Define strings for localization
 const uiStrings = {
     en: {
-        heading: "Select a text object",
-        body: "You'll need to selet a text object to create vertical writing."
+        selectionError: {
+            heading: "Select a text object",
+            body: "You'll need to selet a text object to create vertical writing."
+        },
+        alreadyRan: {
+            heading: "Text is already vertical",
+            body: "The text you've selected is already vertical. Please select a horizontal text object."
+        }
     },
     ja: {
-        heading: "テキストを選択してください。",
-        body: "縦書きに変えるためにテキストを選択する必要があります。"
+        selectionError: {
+            heading: "テキストを選択してください。",
+            body: "縦書きに変えるためにテキストを選択する必要があります。"
+            
+        },
+        alreadyRan: {
+            heading: "縦書きのテキストを選択した",
+            body: "選択したテキストは既に縦書きになっていそうです。横書きのテキストを選択してください。"
+        }
     }
 }
 
 // Main command
 function tategakiShiyo(selection) {
-    
+    const language = application.appLanguage;
+
     // If selection is text
     if (selection.items[0] instanceof Text) {
         // Convert to vertical string
         const textObj = selection.items[0];
-        textObj.text = textObj.text.split("").join("\n");
+        
+        if (isTategaki(textObj.text)) {
+            showAlert(language, "alreadyRan");
+        }
+        else {
+           textObj.text = textObj.text.split("").join("\n");
+        }
     }
     else {
-        showAlert(application.appLanguage);
+        showAlert(language, "selectionError");
     }
 }
 
 // Show an alert with localized strings
-async function showAlert(language) {
-    await alert(uiStrings[language].heading, uiStrings[language].body);
+async function showAlert(language, flag) {
+    await alert(uiStrings[language][flag].heading, uiStrings[language][flag].body);
+}
+
+function isTategaki(text) {
+    return text.split("\n").length >= text.length / 2; 
 }
 
 module.exports = {
